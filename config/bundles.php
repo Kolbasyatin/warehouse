ffc11c9643907279aebff8702d054d4f18275d45
zalex <zalex@zalex.com.ua>
2021-05-12 19:21:17 +0300

    Do some settings for testing

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Filesystem\Filesystem;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
@@ -8,4 +9,12 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
+if (isset($_ENV['APP_FIXTURES_LOAD']) && $_ENV['APP_FIXTURES_LOAD']) {
+    (new Filesystem())->remove(__DIR__.'/../var/cache/test');
+    passthru(sprintf('php "%s/../bin/console" cache:warmup', __DIR__));
+    passthru(sprintf('php "%s/../bin/console" doctrine:schema:drop --force -n', __DIR__));
+    passthru(sprintf('php "%s/../bin/console" doctrine:schema:update --force -n', __DIR__));
+    passthru(sprintf('php "%s/../bin/console" doctrine:fixtures:load -n', __DIR__));
 }

6a45390c90c1f8f4dc54a907337e85cdad71173a
zalex <zavalyuk.a.l@sblogistica.ru>
2021-06-11 17:27:13 +0300

    Add templates

--- src/Controller/DemonstrationController.php
+++ src/Controller/DemonstrationController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('demo')]
 class DemonstrationController extends AbstractController
 {
-    #[Route('/demonstration', name: 'demonstration')]
+    #[Route('/index', name: 'demonstration')]
     public function index(): Response
     {
         return $this->render('demonstration/index.html.twig', [
